9e5118634ff2424eddd6e097721005668b022222
Abdullah Rowaished <abdullah_rowaished@outlook.sa>
2022-06-27 20:42:29 +0300

    experminetal: change image size with resize

--- venv/Scripts/User/workbench.py
+++ venv/Scripts/User/workbench.py
@@ -1,27 +1,28 @@
-background_image_filename = '../sushiplate.jpg'
+background_image_filename = '..\sushiplate.jpg'
 
 import pygame
 from pygame.locals import *
 from sys import exit
 
+SCREEN_SIZE = (1280, 720)
+
 pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE, RESIZABLE, 32)
 
-CATONKEYBOARD = USEREVENT + 1
-cat_event = pygame.event.Event(CATONKEYBOARD, event_msg="Bad cat!")
-pygame.event.post(cat_event)
-
-SCREEN_SIZE = (800, 600)
-screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
 background = pygame.image.load(background_image_filename).convert()
-screen.fill((0, 0, 0))
-screen.blit(background, (0, 0))
-font = pygame.font.SysFont("arial", 16)
 
 while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            exit()
-        elif event.type == CATONKEYBOARD:
-            text = event.event_msg
-            screen.blit(font.render(text, True, (0, 0, 0)), (0, 400))
+
+    event = pygame.event.wait()
+    if event.type == QUIT:
+        exit()
+    if event.type == VIDEORESIZE:
+        SCREEN_SIZE = event.size
+        screen = pygame.display.set_mode(SCREEN_SIZE, RESIZABLE, 32)
+        pygame.display.set_caption("Window resized to "+str(event.size))
+
+    screen_width, screen_height = SCREEN_SIZE
+    Rect.inflate(background.convert(),screen_width,screen_height)
+    screen.blit(background, (0,0))
+
     pygame.display.update()

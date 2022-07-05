1020ae41b415395bd5646886dcffb881d699038a
Abdullah Rowaished <abdullah_rowaished@outlook.sa>
2022-07-05 10:52:20 +0300

    expirement: Joystick Deadzone

--- venv/Scripts/User/Workbench.py
+++ venv/Scripts/User/Workbench.py
@@ -1,63 +1,46 @@
-background_image_filename = '../sushiplate.jpg'
-sprite_image_filename = '../fugu.png'
-
 import pygame
+from math import fabs
 from pygame.locals import *
 from sys import exit
 
 pygame.init()
 
-screen = pygame.display.set_mode((1920, 1080),FULLSCREEN|HWSURFACE,32)
+SCREEN_SIZE = (640, 480)
+screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
 
-background = pygame.image.load(background_image_filename).convert()
-sprite = pygame.image.load(sprite_image_filename)
-Fullscreen = True
+font = pygame.font.SysFont("arial", 16)
+font_height = font.get_linesize()
+event_text = []
 
-# Our clock object
-clock1 = pygame.time.Clock()
-clock2 = pygame.time.Clock()
-
-# X coordinates of our sprite
-x1 = 0.
-x2 = 0.
-# Speed in pixels per second
-speed = 250.
-
-frame_no = 0
+joysticks = []
+for joystick_no in range(pygame.joystick.get_count()):
+    stick = pygame.joystick.Joystick(joystick_no)
+    stick.init()
+    joysticks.append(stick)
 
 while True:
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            exit()
-        if event.type == KEYDOWN:
-            if event.key == K_f:
-                Fullscreen = not Fullscreen
-                if Fullscreen:
-                    screen = pygame.display.set_mode((1920, 1080), FULLSCREEN, 32)
-                else:
-                    screen = pygame.display.set_mode((1280, 720), 0, 32)
-            screen.blit(background, (0, 0))
+    event = pygame.event.wait()
+    if event.type == QUIT:
+        exit()
+    if event.type in (JOYAXISMOTION,
+                      JOYBALLMOTION,
+                      JOYHATMOTION,
+                      JOYBUTTONUP,
+                      JOYBUTTONDOWN):
+        event_text.append(str(event))
 
-    screen.blit(background, (0, 0))
-    screen.blit(sprite, (x1, 50))
-    screen.blit(sprite, (x2, 250))
+    event_text = event_text[-int(SCREEN_SIZE[1]/font_height):]
 
-    tp1 = clock1.tick(30)
-    tp2 = clock2.tick(60)
-    tps1 = tp1 / 1000.0
-    tps2 = tp2 / 1000.0
+    screen.fill((255,255,255))
 
-    dm1 = tps1 * speed
-    dm2 = tps2 * speed
-    x1 += dm1
-    x2 += dm2
-
-    # If the image goes off the end of the screen, move it back
-    if x1 > 640.:
-        x1 -= 640
-    if x2 > 640.:
-        x2 -= 640
+    y = SCREEN_SIZE[1] - font_height
+    for text in reversed(event_text):
+        if (fabs(stick.get_axis(0).real) >= 0.25
+                or fabs(stick.get_axis(1).real) >= 0.25
+                or fabs(stick.get_axis(2).real) >= 0.25
+                or fabs(stick.get_axis(3).real) >= 0.25):
+            screen.blit(font.render(text, True, (0,0,0)), (0,y))
+        y-=font_height
 
     pygame.display.update()
-    frame_no += 1
